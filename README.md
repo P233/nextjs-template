700a097933b620619f00dfbf93256c5dd887e307
Peiwen Lu <hi@peiwen.lu>
2021-11-10 12:42:46 +0800

    Add google fonts

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 1. Click the `Use this template` button to create your own repo.
 2. Open the `package.json` file, update the project name, and then run `npm i`.
-3. Run `npm run generate ComponentName` to generate a new component folder.
+3. Update your Google font in `./src/pages/_document.tsx` and `./src/styles/_common.scss`.
+4. Run `npm run generate ComponentName` to generate a new component folder.
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,13 @@ class MyDocument extends Document {
         <Head>
           {/* Favicon */}
           <link rel="shortcut icon" href="/favicon.ico" />
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
