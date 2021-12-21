7c475472f574c9593d3ba7e291bb612b3b78d849
Peiwen Lu <hi@peiwen.lu>
2021-12-21 19:11:16 +0800

    Remove sass includePaths

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const path = require("path");
-
 module.exports = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "src/styles")]
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
