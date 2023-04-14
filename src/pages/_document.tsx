2225b9ce029e0750cc730df7e9ae8f4642057eee
Peiwen Lu <hi@peiwen.lu>
2023-04-14 15:29:18 +0800

    Update crossOrigin attribute

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.ico" />
           {/* Google Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
             rel="stylesheet"
